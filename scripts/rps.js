40f36e5293b11785cd8bd1fead8a90262ae3b725
kuriroley <christian.rodriguez@tutanota.com>
2023-08-15 21:48:45 +0900

    .

--- scripts/rps.js
+++ scripts/rps.js
@@ -3,24 +3,12 @@ function draw(){
     if (canvas.getContext) {
         const ctx = canvas.getContext('2d');
 
-        ctx.fillStyle = 'black';
-        ctx.fillRect(10,10,80,80);
+        ctx.fillRect(25,25,100,100);
+        ctx.clearRect(25,25,80,80);
 
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-        ctx.fillRect(20,20,80,80);
-
-        ctx.fillStyle = "rgba(10, 20, 100, 0.5)";
-        ctx.fillRect(30,30,80,80)
-
-        ctx.fillStyle = 'rgba(80, 20, 300, 0.5)';
-        ctx.fillRect(40,40,80,80);
-
-        ctx.beginPath();
-        ctx.moveTo(60,60);
-        ctx.lineTo(150,200);
-        ctx.lineTo(200,250);
-        ctx.fill();
-
+        ctx.fillRect(25,250,100,100);
+        ctx.clearRect(25,250,50,50);
+        ctx.strokeRect(25,250,100,100);
         
     }
 }
